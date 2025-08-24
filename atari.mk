c67ce7eb6bd0d1a40dc54cb07ebfc3b2d2cfeabb
Adam Minchinton <adam.minchinton@gmail.com>
2025-08-24 16:57:46 +0200

    Branch update for Atari

--- atari.mk
+++ atari.mk
@@ -2,7 +2,7 @@
 .PHONY: atari_all atari_body atari_deps atari_clone atari_autogen atari_configure atari_build atari_install atari_copy_config
 
 ATARI_REPO_URL := https://github.com/aminch/atari800-pios-lite.git
-ATARI_BRANCH   := pios-lite
+ATARI_BRANCH   := pi-os-lite
 ATARI_SRC_ROOT := $(HOME)/atari-src
 ATARI_BUILD_DIR := $(ATARI_SRC_ROOT)/atari800-pios-lite
 ATARI_INSTALL_DIR := $(HOME)/atari800
@@ -14,7 +14,7 @@ ATARI_DEPS = libsdl2-image-dev libsdl2-dev libsdl2-2.0-0
 atari_deps: ## Install Atari specific dependencies
 	sudo apt-get install -y $(ATARI_DEPS)
 
-atari_clone: ## Clone / update Atari800 repo (pios-lite branch)
+atari_clone: ## Clone / update Atari800 repo (pi-os-lite branch)
 	mkdir -p $(ATARI_SRC_ROOT)
 	@if [ ! -d "$(ATARI_BUILD_DIR)/.git" ]; then \
 		git clone --branch $(ATARI_BRANCH) --depth 1 $(ATARI_REPO_URL) $(ATARI_BUILD_DIR); \
